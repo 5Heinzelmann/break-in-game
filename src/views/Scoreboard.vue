97118216b0d8fadc1990569c1fef8e61bb4aa91a
Mirco Heinzelmann <mirco.heinzelmann@iteratec.com>
2020-05-12 15:00:35 +0200

    scoreboard vue

---
+++ src/views/Scoreboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="board">
+        board
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "ScoreBoard",
+        data() {
+            return {}
+        },
+        mounted() {
+
+        },
+        computed: {},
+        methods: {}
+    };
+</script>
+
+<style lang="scss">
+    .board {
+        color: #ffffff;
+        background-color: #262c31;
+        display: flex;
+        justify-content: center;
+    }
+</style>
