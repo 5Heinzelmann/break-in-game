633f28ecd4fb886285b01391ace1a71920038381
Mirco Heinzelmann <mirco.heinzelmann@iteratec.com>
2020-05-12 17:10:59 +0200

    minor fixes

--- src/views/CodeChallenge.vue
+++ src/views/CodeChallenge.vue
@@ -4,7 +4,6 @@
 
         <ImplementCode/>
 
-        <div class="output">{{ output }}</div>
         <router-link to="/score">Show Scoreboard</router-link>
     </div>
 </template>
@@ -17,9 +16,6 @@
         name: "CodeChallenge",
         components: {
             ImplementCode
-        },
-        mounted() {
-            this.loadExample();
         },
         computed: {
             task() {
@@ -41,15 +37,6 @@
         align-items: center;
         height: 100vh;
         width: 100vw;
-
-        .output {
-            color: #4db37c;
-            background-color: #09141c;
-            font-family: monospace;
-            font-weight: bold;
-            padding: 16px;
-            font-size: 16px;
-        }
 
         .task {
             color: #ffffff;
