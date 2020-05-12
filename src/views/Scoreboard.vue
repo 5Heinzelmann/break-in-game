6bb5bd536666431063455329f8169e385bb2b84b
Mirco Heinzelmann <mirco.heinzelmann@iteratec.com>
2020-05-12 15:43:21 +0200

    store score

--- src/views/Scoreboard.vue
+++ src/views/Scoreboard.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="board">
+        <div>
+            <q-input v-model="name" label="Your name"/>
+            <q-input type="number" v-model="score" label="Your score"/>
+
+            <q-btn color="primary" :disable="name === '' || score < 0" @click="submitScore()">Submit</q-btn>
+        </div>
         <q-table
-                square
                 title="Scoreboard"
                 :data="scores"
                 :columns="columns"
-                row-key="name"
+                row-key="id"
                 :pagination.sync="pagination"
                 hide-bottom
         />
@@ -18,6 +23,8 @@
         name: "ScoreBoard",
         data() {
             return {
+                name: '',
+                score: null,
                 scores: [],
                 columns: [
                     {name: 'name', label: 'Username', field: 'name', align: 'left'},
@@ -32,22 +39,39 @@
             }
         },
         mounted() {
-            this.scores = [
-                {name: 'Luke', score: 6000},
-                {name: 'Bob', score: 8000},
-                {name: 'Hieu', score: 9000},
-                {name: 'Lisa', score: 7000},
-            ]
+            if (localStorage.scores) {
+                this.scores = JSON.parse(localStorage.scores)
+            }
         },
         computed: {},
-        methods: {}
+        methods: {
+            submitScore() {
+                if (this.name !== '' && this.score >= 0 && this.score < Number.MAX_SAFE_INTEGER) {
+                    this.scores.push({
+                        id: this.uuidv4(),
+                        name: this.name,
+                        score: this.score
+                    });
+                    this.scores = this.scores.sort((a, b) => {
+                        return a.score < b.score
+                    });
+                    localStorage.scores = JSON.stringify(this.scores);
+                    this.name = '';
+                    this.score = '';
+                }
+            },
+            uuidv4() {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                });
+            }
+        },
     };
 </script>
 
 <style lang="scss">
     .board {
-        color: #ffffff;
-        background-color: #262c31;
         display: flex;
         justify-content: center;
     }
