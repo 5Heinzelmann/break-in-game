e7616666da6b46e3b465d219ee41f816983a31e1
mmakkai <marcel.makkai@iteratec.com>
2020-07-22 14:01:02 +0200

    move stuff to one Component, code refactoring, auto load for q select, new discussed design

--- src/views/CodeChallenge.vue
+++ src/views/CodeChallenge.vue
@@ -1,10 +1,54 @@
 <template>
-    <div class="code-challenge">
-        <div class="grid-container"></div>
-        <div class="grid-headerline"></div>
+    <!-- TODO to middle -->
+    <div class="code-challenge ">
         <div class="task">{{ task }}</div>
+            
+            <div class="controls">
+            <q-btn
+                    class= "btn"
+                    label="Hints for time penalty"
+            />
 
-        <ImplementCode/>
+            <q-select
+                    v-model="selectedLang"
+                    :options="langOptions"
+                    label="Language"
+                    filled
+                    square
+                    bg-color="white"
+                    class="lang-select"
+                    @input="loadExample()"
+            />
+        </div>            
+
+        <!-- TODO Styling of Code Input? -->
+        <codemirror v-model="input" :options="cmOptions"/>
+
+        <div class="controls">
+            <q-btn
+                    class= "btn"
+                    label="RESET"
+            />
+
+            <!-- TODO background color -->
+            <q-input 
+                square
+                filled
+                v-model="text"
+                label="Please set your input"
+            />
+
+            <div @click="executeCode()" class="btn">
+                <span class="text-h4"><b>{{ buttonLabel }}</b></span>
+            </div>
+        </div>        
+
+        <div class="output">{{ output }}</div>
+
+        <q-btn
+                class= "btn"
+                label="Final Test (on Success -> Show Scorecard)"
+        />
 
         <router-link to="/score">Show Scoreboard</router-link>
     </div>
@@ -12,35 +56,109 @@
 
 <script>
     import jsonChallenges from "../challenges/challenge.json";
-    import ImplementCode from '@/components/ImplementCode.vue';
+    // import ImplementCode from '@/components/ImplementCode.vue';
+    const axios = require("axios");
+    import "codemirror/theme/base16-dark.css";
 
     export default {
         name: "CodeChallenge",
-        components: {
-            ImplementCode
+        // components: {
+        //     ImplementCode
+        // },
+        data() {
+            const langs = jsonChallenges.languages.map(lang => ({
+                label: lang.name,
+                value: lang.id,
+                example: lang.example
+            }));
+
+            return {
+                output: 'No Output yet',
+                url: "https://api.judge0.com/submissions/",
+                interval: null,
+                input: "",
+                javaScriptExample: 'const x = "moin";\n' + "console.log(x, x);",
+                javaExample:
+                    "class Main {\n" +
+                    "    public static void main(String[] args) {\n" +
+                    '        System.out.println("Hello, Java ;)");\n' +
+                    "    }\n" +
+                    "}",
+                finished: true,
+                buttonLabel: "Execute",
+                selectedLang: langs.length > 0 ? langs[0] : null,
+                langOptions: langs,
+                cmOptions: {
+                    tabSize: 4,
+                    mode: "text/javascript",
+                    theme: "base16-dark",
+                    lineNumbers: true,
+                    line: true
+                }
+            };
+        },
+        mounted() {
+            this.loadExample();
         },
         computed: {
+            selectedLanguageId() {
+                return this.selectedLang ? this.selectedLang.value : null;
+            },
             task() {
                 const allTasks = jsonChallenges.challenges.map(item => {
                     return item.task;
                 });
                 return allTasks.length !== 0 ? allTasks[0] : null;
             }
+        },
+        methods: {
+            executeCode() {
+                if (this.finished) {
+                    this.finished = false;
+                    this.buttonLabel = "Wait...";
+                    const data = {
+                        source_code: this.input,
+                        language_id: this.selectedLanguageId
+                    };
+
+                    axios.post(this.url, data).then(resp => {
+                        const token = resp.data.token;
+
+                        this.interval = setInterval(() => {
+                            this.fetchResult(token);
+                        }, 500);
+                    });
+                }
+            },
+            fetchResult(token) {
+                axios.get(this.url + token).then(resp => {
+                    if (resp.data.status.id > 2) {
+                        clearInterval(this.interval);
+                    }
+                    this.output = resp.data.stdout
+                        ? "> " + resp.data.stdout
+                        : "> " + resp.data.status.description;
+                    this.finished = true;
+                    this.buttonLabel = "Execute";
+                });
+            },
+            loadExample() {
+                this.input = this.selectedLang ? this.selectedLang.example : "";
+            }
         }
     };
 </script>
 
 <style lang="scss">
-
+    
     .code-challenge {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        align-items: center;
         height: 100vh;
-        width: 100vw;
+        width: 25%;
 
-        .task {
+        .output {
             color: #ffffff;
             background-color: #09141c;
             font-family: monospace;
@@ -48,20 +166,50 @@
             padding: 16px;
             font-size: 16px;
         }
-        
-        .grid-container {
-            display: grid;
-            grid-template-columns: 20% 20% 20% 20% 20%;
-            grid-template-rows: 15% 40% 15% 30%;            
+
+        .controls {
+            display: flex;
+            justify-content: space-between;
+            margin: 10px 0;
+
+            .lang-select {
+                font-size: 20px;                
+                width: 150px;
+            }
         }
-        
-        .grid-headerline {
-            align-self: stretch;
-            // grid-column-start: 1;
-            // grid-column-end: span col4-start;
-            // grid-row-start: 2;
-            // grid-row-end: span 2;
+
+        .input {
+            width: 700px;
+            height: 300px;
+            border: 0;
+            padding: 8px 16px;
+            line-height: 35px;
+        }
+
+        .btn {
+            display: flex;
+            background-color: #a9218e;
+            color: #09141c;
+            cursor: pointer;
+            justify-content: center;
+            align-items: center;
+            padding: 10px;
+            margin: 12px 0 14px 0;
+        }
+
+        .btn:hover {
+            background-color: #d329b2;
+        }
+
+        .task {
+            color: #ffffff;
+            background-color: #09141c;
+            font-family: monospace;
+            font-weight: bold;
+            text-align: center;
+            padding: 16px;
+            font-size: 16px;
+            width: 100%;
         }
     }
-
 </style>
